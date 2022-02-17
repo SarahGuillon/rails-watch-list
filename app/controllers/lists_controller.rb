cac3cf9eb1948b7e813f93ab0e5a8de4b7bdb3e9
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-02-17 15:05:47 +0100

    Lists routes : action index show create new

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.where(list_id: params[:id])
+    @movies = Movie.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+
+end
